a573177f6816f608a35a7baf0d77adad27bc1d25
dy4605-srm <dy4605@srmist.edu.in>
2025-11-10 23:13:43 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int dar[1000];
+	int s, i;
+	
+	// printf("the length of the array is" );
+	// scanf("%d", &s);
+	
+	for (i=0; i<6; i++)
+	{
+		printf("enter the given no. ");
+		scanf ("%d", dar[i]);
+    }	
+	for (i=0; i < 6; i++)
+	{
+		printf ("%d ", dar[i]);
+	
+	}
+}
